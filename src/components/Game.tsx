75d0267a2e1b3fd643273440d117a49886a0aad3
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-03-10 22:37:18 +0900

    init project

---
+++ src/components/Game.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+const Game = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Game;
---
+++ src/components/Ready.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+
+const Ready = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Ready;
